00c8ab10c79cf248165bdcd7eaa4a4be7252235e
renanluizmacedo <renanluizmacedo@gmail.com>
2021-06-13 23:54:23 -0300

    acessando via Jquery/Ajax

---
+++ resources/views/produtos.blade.php
@@ -0,0 +1,154 @@
+@extends('layout.app', ["current" => "produtos" ])
+
+@section('body')
+    <div class="card border">
+        <div class="card-body">
+            <h5 class="card-title">Cadastro de Produtos</h5>
+    
+            <table class="table table-ordered table-hover" id ="tabelaProdutos">
+                <thead>
+                    <tr>
+                        <th>Código</th>
+                        <th>Nome</th>
+                        <th>Quantidade</th>
+                        <th>Preço</th>
+                        <th>Departmaneto</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+       
+                </tbody>
+            </table>
+        </div>
+        <div class="card-footer">
+            <button class="btn btn-sm btn-primary" role="button" onclick="novoProduto()">Novo Produto</button>
+        </div>
+    </div> 
+    <div class="modal" tabindex = "-1" role="dialog" id="dlgProdutos" >
+        <div class="modal-dialog" role = "document">
+            <div class="modal-content">
+                <form class = "form-horizontal" id="form-produto">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Novo Produto</h5>
+                    </div>
+                    <div class="modal-body">
+
+
+                        <input type="hidden" id="id" class = "form-control">
+
+                        <div class="form-group">
+
+                            <label for="nomeProduto" class="control-label">Nome Do Produto</label>
+                            <div class="input-group">
+                                <input type="text" class = "form-control" id="nomeProduto" 
+                                placeholder="Nome do Produto"> 
+                            </div>
+
+                        </div>
+
+                        <div class="form-group">
+
+                            <label for="precoProduto" class="control-label">Preço Do Produto</label>
+                            <div class="input-group">
+                                <input type="number" class = "form-control" id="precoProduto" 
+                                placeholder="Preço do Produto"> 
+                            </div>
+
+                        </div>
+
+                        <div class="form-group">
+
+                            <label for="quantidadeProduto" class="control-label">Quantidade Do Produto</label>
+                            <div class="input-group">
+                                <input type="number" class = "form-control" id="quantidadeProduto" 
+                                placeholder="Quantidade do Produto"> 
+                            </div>
+
+                        </div>
+
+                        <div class="form-group">
+
+                            <label for="categoriaProduto" class="control-label">Categoria Produto</label>
+                            <div class="input-group">
+                                <select class = "form-control" id="categoriaProduto"> 
+                                </select>
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <div class="modal-footer">
+
+                        <button type="submit" class="btn btn-primary">Salvar</button>
+                        <button type="cancel" class="btn btn-secondary" data-dissmiss="modal">Cancelar</button>
+
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
+
+
+@section('javascript')
+    <script type="text/javascript">
+
+
+        function novoProduto(){
+           
+            $('#nomeProduto').val('');
+            $('#precoProduto').val('');
+            $('#quantidadeProduto').val('');
+            $('#categoriaProduto').val('');
+
+            $('#dlgProdutos').modal('show');
+        }
+
+        function carregarCategorias(){
+           
+            $.getJSON('/api/categorias', function(data){
+
+                for(i=0;i<data.length;i++){
+                    opcao = '<option value = "' +data[i].id+'">' + data[i].nome + '</option';
+                    $('#categoriaProduto').append(opcao);
+                }
+            });
+
+        }
+        function montarLinha(p){
+            var linha = "<tr>" + 
+                    "<td>" + p.id +"</td>" +
+                    "<td>" + p.nome +"</td>" +
+                    "<td>" + p.estoque +"</td>" +
+                    "<td>" + p.preco +"</td>" +
+                    "<td>" + p.categoria_id +"</td>" +
+                    "<td>" +
+                        '<button class = "btn btn-sm btn-primary"> Editar</button> '+
+                        '<button class = "btn btn-sm btn-danger"> Apagar</button> '+
+                    "</td>" +
+                "</tr>";
+
+            return linha;
+
+        }
+        function carregarProdutos(){
+           
+            $.getJSON('/api/produtos', function(produtos){
+
+                for(i=0;i<produtos.length;i++){
+                    linha = montarLinha(produtos[i]);
+                    $('#tabelaProdutos>tbody').append(linha);
+                }
+            });
+
+        }
+        $(function(){
+            carregarCategorias();
+            carregarProdutos();
+        })
+
+        
+
+    </script>
+@endsection
---
+++ routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ControladorCategoria;
+use App\Http\Controllers\ControladorProduto;
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/categorias',[ControladorCategoria::class,'indexJson']);
+Route::resource('/produtos',ControladorProduto::class);
